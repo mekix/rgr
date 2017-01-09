1101815994fb7abbcd3fb15947d4aad0ae699d23
comunidad de software libre de la uni <csluni@debian>
2017-01-09 23:08:30 +0200

    Adding static properties and also babel-stage-0

--- js/components/Main.js
+++ js/components/Main.js
@@ -6,7 +6,7 @@ let _getAppState = () => {
     return {links: LinkStore.getAll()};
 }
 
-export default class Main extends React.Component {
+class Main extends React.Component {
 
     static propTypes = {
         limit: React.PropTypes.number
@@ -15,12 +15,9 @@ export default class Main extends React.Component {
     static defaultProps = {
         limit: 4
     }
-    constructor(props){
-        super(props);
-        this.state = _getAppState();
-        this.onChange = this.onChange.bind(this);
-    }
-	//componentWillMount(){	}
+
+    state = _getAppState();
+
 	componentDidMount(){
 		API.fetchLinks();
         LinkStore.on("change", this.onChange);
@@ -28,17 +25,16 @@ export default class Main extends React.Component {
     componentWillUnmount(){
         LinkStore.removeListener("change", this.onChange);
     }
-    onChange(){
-        console.log("4. In the View");
+    onChange = () => {
         this.setState(_getAppState());
     }
 	render() {
-
         let content = this.state.links.slice(0, this.props.limit).map(link => {
             return <li key={link._id}>
                 <a href={link.url}>{link.title}</a>
             </li>;
-        })
+        });
+
     return (
     	<div>
     		<h3>Links</h3>
@@ -50,12 +46,4 @@ export default class Main extends React.Component {
   }
 }
 
-/**
-Main.propTypes = {
-    limit: React.PropTypes.number
-}
-
-Main.defaultProps = {
-    limit: 4
-}
-**/
+export default  Main;
--- server.js
+++ server.js
@@ -8,25 +8,14 @@ let app = express();
 app.use(express.static('public'));
 
 
-let db;
+(async () => {
+	let db = await MongoClient.connect("mongodb://csluni:csluni@ds155028.mlab.com:55028/csluni");
 
-MongoClient.connect("mongodb://csluni:csluni@ds155028.mlab.com:55028/csluni", (err, database) => {
-	if(err) throw err;
-
-	db =  database;
 	app.use('/graphql', GraphQLHTTP({
 		schema: schema(db),
 		graphiql: true
 	}));
 
 	app.listen(3000, () => console.log("Listening on port 3000"));	
-});
-
-//app.get("/data/links", (req, res) => {
-//
-//	db.collection("links").find({}).toArray((err, links) => {
-//		if(err) throw err;
-//
-//		res.json(links);
-//	})
-//});
+	
+})();
